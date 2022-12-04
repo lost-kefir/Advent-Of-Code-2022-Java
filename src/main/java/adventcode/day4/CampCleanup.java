2e3ceb919a14ce87bf0b56cc7324f08f8306992d
piotrwoz <p.wozniak@tuta.io>
2022-12-04 11:25:45 +0100

    Added puzzle solution for day4

---
+++ src/main/java/adventcode/day4/CampCleanup.java
@@ -0,0 +1,56 @@
+package adventcode.day4;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.tuple.Pair;
+import reader.TestInputReader;
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+/**
+ * <a href="https://adventofcode.com/2022/day/4">Day 4 puzzle</a>
+ */
+
+@Slf4j
+public class CampCleanup {
+    @Getter
+    private final String assignments;
+    private final static int LEFT = 0;
+    private final static int RIGHT = 1;
+
+    public CampCleanup(@NonNull String inputFileName) {
+        assignments = new TestInputReader(inputFileName).getInputAsString();
+    }
+
+    public Pair<List<Integer>, List<Integer>> makeAssignmentPair(@NonNull String pairAssignment) {
+        String[] assignments = pairAssignment.split(",");
+        List<List<Integer>> fullAssignment = new LinkedList<>();
+        for (String assignment: assignments) {
+            int[] range = Arrays.stream(assignment.split("-"))
+                    .mapToInt(Integer::valueOf).toArray();
+            int from = range[LEFT];
+            int to = range[RIGHT];
+            if (from > to || from == 0 || to == 0)
+                throw new IllegalArgumentException(
+                        String.format("Range should be between lower and upper limits on a scale: %d - %d", from, to));
+            fullAssignment.add(IntStream.rangeClosed(from, to)
+                    .boxed().toList());
+        }
+
+        var assignmentsPair = Pair.of(fullAssignment.get(LEFT), fullAssignment.get(RIGHT));
+        log.debug("Assignment Pair: {}", assignmentsPair);
+        return assignmentsPair;
+    }
+
+    public boolean isAssignmentOverlapping(Pair<List<Integer>, List<Integer>> assignmentPair) {
+        return assignmentPair.getLeft().stream()
+                .anyMatch(assignmentPair.getRight()::contains);
+    }
+
+    public boolean isAssignmentsFullyOverlapping(Pair<List<Integer>, List<Integer>> assignmentPair) {
+        return new HashSet<>(assignmentPair.getLeft()).containsAll(assignmentPair.getRight())
+                || new HashSet<>(assignmentPair.getRight()).containsAll(assignmentPair.getLeft());
+    }
+}
---
+++ src/test/java/Day4CampCleanupTest.java
@@ -0,0 +1,84 @@
+import adventcode.day4.CampCleanup;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * <a href="https://adventofcode.com/2022/day/4">Day 4 puzzle</a>
+ */
+
+@Slf4j
+public class Day4CampCleanupTest {
+
+    private final String inputFileName = "day4_test_input.txt";
+    private final CampCleanup organizer = new CampCleanup(inputFileName);
+
+    @Test //In how many assignment pairs does one range fully contain the other?
+    public void checkNumberOfPairsWithRangeFullyOverlaps() {
+        var expectedResult = 2;
+        var numberOfOverlappingAssignments = new AtomicInteger();
+        var assignments = organizer.getAssignments();
+        assignments.lines().forEach(assignment -> {
+            var assignmentPair = organizer.makeAssignmentPair(assignment);
+            if (organizer.isAssignmentsFullyOverlapping(assignmentPair))
+                numberOfOverlappingAssignments.incrementAndGet();
+        });
+
+        log.debug("Number of pairs that one range fully contain the other: {}",
+                numberOfOverlappingAssignments.get());
+        assertEquals(expectedResult, numberOfOverlappingAssignments.get());
+    }
+
+    @Test //In how many assignment pairs do the ranges overlap?
+    public void checkNumberOfPairsWithRangePartiallyOverlaps() {
+        var expectedResult = 4;
+        var numberOfOverlappingAssignments = new AtomicInteger();
+        var assignments = organizer.getAssignments();
+
+        assignments.lines().forEach(assignment -> {
+            var assignmentPair = organizer.makeAssignmentPair(assignment);
+            if (organizer.isAssignmentOverlapping(assignmentPair))
+                numberOfOverlappingAssignments.incrementAndGet();
+        });
+
+        log.debug("Number of pairs with range overlap: {}",
+                numberOfOverlappingAssignments.get());
+        assertEquals(expectedResult, numberOfOverlappingAssignments.get());
+    }
+
+    @Test
+    public void checkAssignmentPairMapping() {
+        var range = "1-9,10-100";
+        var expectedSmallerRange = IntStream.rangeClosed(1,9).boxed().toList();
+        var expectedLongRange = IntStream.rangeClosed(10,100).boxed().toList();;
+        var assignmentPair = organizer.makeAssignmentPair(range);
+
+        assertEquals(expectedSmallerRange, assignmentPair.getLeft());
+        assertEquals(expectedLongRange, assignmentPair.getRight());
+    }
+
+    @Test
+    public void checkAssignmentRangeValidation() {
+        var range = "9-1,1-9";
+        var msg = String
+                .format("Range should be between lower and upper limits on a scale: %d - %d", 9, 1);
+        Exception exception = assertThrows(IllegalArgumentException.class,
+                () -> organizer.makeAssignmentPair(range));
+        assertEquals(msg, exception.getMessage());;
+    }
+
+    @Test
+    public void checkAssignmentRangeZeroValidation() {
+        var range = "0-1,1-0";
+        var msg = String
+                .format("Range should be between lower and upper limits on a scale: %d - %d", 0, 1);
+        Exception exception = assertThrows(IllegalArgumentException.class,
+                () -> organizer.makeAssignmentPair(range));
+        assertEquals(msg, exception.getMessage());;
+    }
+}
