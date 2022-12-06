3675362dae51ef5014a63d173e0aa634614e6512
kefir <p.wozniak@tuta.io>
2022-12-06 13:47:10 +0100

    Added puzzle solution for day6

---
+++ src/main/java/adventcode/day6/TuningTrouble.java
@@ -0,0 +1,42 @@
+package adventcode.day6;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import reader.TestInputReader;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Slf4j
+public class TuningTrouble {
+    @Getter
+    private final String signal;
+
+    public TuningTrouble(@NonNull String inputFileName) {
+        signal = new TestInputReader(inputFileName).getInputAsString();
+    }
+
+    public Integer markerIn(String line, int markerLength) {
+        char[] chars = line.toCharArray();
+        
+        for (int c = 0; c < chars.length; c++) {
+            if (markerLength > chars.length)
+                markerLength = (chars.length - c);
+
+            var part = line.substring(c, c + markerLength);
+            
+            if (isUniqueMarker(part)) {
+                return c + markerLength;
+            }
+        }
+        return null;
+    }
+
+    private boolean isUniqueMarker(String string) {
+        Set<Character> charSet = string.chars()
+                .mapToObj(i -> (char) i)
+                .collect(Collectors.toSet());
+        return charSet.size() == string.length();
+    }
+}
---
+++ src/test/java/Day6TuningTroubleTest.java
@@ -0,0 +1,41 @@
+import adventcode.day6.TuningTrouble;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Slf4j
+public class Day6TuningTroubleTest {
+
+    private final String inputFileName = "day6_test_input.txt";
+    private final TuningTrouble resolver = new TuningTrouble(inputFileName);
+
+    @Test //How many characters need to be processed before the first start-of-packet marker is detected?
+    public void checkForUniqueSignalMarkers() {
+        int markerLength = 4;
+        List<Integer> expectedResult = List.of(7, 5, 6, 10, 11);
+        List<Integer> actualResult = new ArrayList<>();
+        
+        resolver.getSignal().lines().forEach(line -> actualResult.add(resolver.markerIn(line, markerLength)));
+
+        log.debug("Marker appear after characters: {}",
+                actualResult);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test //How many characters need to be processed before the first start-of-message marker is detected?
+    public void checkForUniqueMessageMarkers() {
+        int messageMarkerLength = 14;
+        List<Integer> expectedResult = List.of(19, 23, 23, 29, 26);
+        List<Integer> actualResult = new ArrayList<>();
+
+        resolver.getSignal().lines().forEach(line -> actualResult.add(resolver.markerIn(line, messageMarkerLength)));
+
+        log.debug("Marker appear after characters: {}",
+                actualResult);
+        assertEquals(expectedResult, actualResult);
+    }
+}
